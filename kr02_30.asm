29c3c4e182022a723374ed68f6d67510bea6159f
vlad_bravo <17.bravo@gmail.com>
2026-01-06 23:21:26 +0500

    some directives

--- kr02_30.asm
+++ kr02_30.asm
@@ -1,5 +1,15 @@
+.setting "OmitUnusedFunctions", true
 .target "8080"
 .format "bin"
-.org 0x8800
+.org 0xF800
 
     jmp *
+
+.function _CALL()
+    nop
+.endfunction
+
+
+.align 0x0800,0xFF
+
+.end
